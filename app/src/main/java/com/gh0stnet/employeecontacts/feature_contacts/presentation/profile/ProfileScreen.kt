beec02e3d986d12a313ee4b40c524e8b0f147704
Gh0st <rainbaker@gmail.com>
2022-11-19 02:25:05 +1100

    Added feature clickable email and phone number in profile screen with context

--- app/src/main/java/com/gh0stnet/employeecontacts/feature_contacts/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/com/gh0stnet/employeecontacts/feature_contacts/presentation/profile/ProfileScreen.kt
@@ -1,6 +1,8 @@
 package com.gh0stnet.employeecontacts.feature_contacts.presentation.profile
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
@@ -16,6 +18,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.FabPosition
 import androidx.compose.material.FloatingActionButton
@@ -42,11 +45,15 @@ import androidx.compose.ui.graphics.ImageShader
 import androidx.compose.ui.graphics.ShaderBrush
 import androidx.compose.ui.graphics.TileMode
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import com.gh0stnet.employeecontacts.R
 import com.gh0stnet.employeecontacts.feature_contacts.domain.model.People
 import com.gh0stnet.employeecontacts.feature_contacts.presentation.destinations.AddEditScreenDestination
@@ -65,6 +72,7 @@ fun ProfileScreen(
     navigator: DestinationsNavigator
 ) {
 
+    val context = LocalContext.current
     val systemUiController = rememberSystemUiController()
     SideEffect {
         systemUiController.setStatusBarColor(
@@ -100,144 +108,172 @@ fun ProfileScreen(
             ) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = "Add contact")
             }
-        },
-        content = {
-            Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.surface) {
-                Column(modifier = Modifier
+        }
+    ) {
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.surface) {
+            Column(
+                modifier = Modifier
                     .verticalScroll(
                         enabled = true,
-                        state = ScrollState(initial = DEFAULT_BUFFER_SIZE))
-                    .background(if (isSystemInDarkTheme()) Charcoal else Color.White)) {
-                    Surface(
-                        modifier = Modifier.height(320.dp),
-                        color = if (isSystemInDarkTheme()) Charcoal else Color.White
-                    ) {
+                        state = ScrollState(initial = DEFAULT_BUFFER_SIZE)
+                    )
+                    .background(if (isSystemInDarkTheme()) Charcoal else Color.White)
+            ) {
+                Surface(
+                    modifier = Modifier.height(320.dp),
+                    color = if (isSystemInDarkTheme()) Charcoal else Color.White
+                ) {
 
-                        val image = ImageBitmap.imageResource(R.drawable.redbg)
-                        val brush = remember(image) {
-                            ShaderBrush(
-                                ImageShader(
-                                    image,
-                                    TileMode.Repeated,
-                                    TileMode.Repeated,
-                                )
+                    val image = ImageBitmap.imageResource(R.drawable.redbg)
+                    val brush = remember(image) {
+                        ShaderBrush(
+                            ImageShader(
+                                image,
+                                TileMode.Repeated,
+                                TileMode.Repeated,
                             )
-                        }
-                        Box(
-                            Modifier
-                                .fillMaxWidth()
-                                .absoluteOffset(0.dp, (-100).dp)
-                                .background(brush)
-                        ) {
-                        }
-                        Box(
-                            Modifier
-                                .fillMaxSize()
-
-                        ) {
-                            Image(
-                                painter = painterResource(id = R.drawable.profile_pic1),
-                                contentDescription = "Profile pic",
-                                contentScale = ContentScale.Crop,
-                                modifier = Modifier
-                                    .size(260.dp)
-                                    .clip(CircleShape)
-                                    .align(Alignment.BottomCenter)
-                            )
-                        }
-                    }
-
-                    Spacer(modifier = Modifier.height(30.dp))
-                    Column(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(
-                            letterSpacing = 5.sp,
-                            text = "${people.firstName} ${people.lastName}",
-                            fontSize = 30.sp,
-
-                            fontWeight = FontWeight.Bold,
-                            color = if (isSystemInDarkTheme()) Color.LightGray else Color.Black
                         )
-                        Text(
-                            text = people.dept,
-                            fontSize = 14.sp,
-                            color = if (isSystemInDarkTheme()) Color.LightGray else Color.Black
-                        )
-
                     }
-                    Spacer(modifier = Modifier.size(20.dp))
-                    Row(
-                        modifier = Modifier
+                    Box(
+                        Modifier
                             .fillMaxWidth()
-                            .padding(75.dp, 20.dp, 50.dp, 10.dp)
+                            .absoluteOffset(0.dp, (-100).dp)
+                            .background(brush)
                     ) {
-                        Text(
-                            text = "Phone",
-                            color = LightOrange,
-                            modifier = Modifier.weight(3f),
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 18.sp
-                        )
-                        Text(
-                            text = "Email",
-                            color = LightOrange,
-                            modifier = Modifier.weight(2f),
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 18.sp
+                    }
+                    Box(
+                        Modifier
+                            .fillMaxSize()
+
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.profile_pic1),
+                            contentDescription = "Profile pic",
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .size(260.dp)
+                                .clip(CircleShape)
+                                .align(Alignment.BottomCenter)
                         )
                     }
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(75.dp, 0.dp, 50.dp, 0.dp)
-                    ) {
-                        Text(
-                            text = people.phoneNumber,
-                            color = if (isSystemInDarkTheme()) Color.LightGray else Color.Black,
-                            modifier = Modifier.weight(3f)
-                        )
-                        Text(
-                            text = people.dept,
-                            color = if (isSystemInDarkTheme()) Color.LightGray else Color.Black,
-                            modifier = Modifier.weight(2f)
-                        )
-                    }
-                    Spacer(modifier = Modifier.size(20.dp))
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(75.dp, 20.dp, 50.dp, 10.dp)
-                    ) {
-                        Text(
-                            text = "Address",
-                            color = LightOrange,
-                            modifier = Modifier.weight(3f),
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 18.sp
-                        )
-                    }
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(75.dp, 0.dp, 50.dp, 0.dp)
-                    ) {
-                        Text(
-                            text = "${people.address}, ${people.state} ${people.country}",
-                            color = if (isSystemInDarkTheme()) Color.LightGray else Color.Black,
-                            modifier = Modifier.weight(3f)
-                        )
-                    }
-                    Image(
-                        painter = painterResource(id = R.drawable.logo),
-                        contentDescription = "Logo",
-                        modifier = Modifier
-                            .size(150.dp)
-                            .padding(75.dp, 0.dp, 0.dp, 0.dp)
+                }
+
+                Spacer(modifier = Modifier.height(30.dp))
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        letterSpacing = 5.sp,
+                        text = "${people.firstName} ${people.lastName}",
+                        fontSize = 30.sp,
+
+                        fontWeight = FontWeight.Bold,
+                        color = if (isSystemInDarkTheme()) Color.LightGray else Color.Black
+                    )
+                    Text(
+                        text = people.dept,
+                        fontSize = 14.sp,
+                        color = if (isSystemInDarkTheme()) Color.LightGray else Color.Black
+                    )
+
+                }
+                Spacer(modifier = Modifier.size(20.dp))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(75.dp, 20.dp, 50.dp, 10.dp)
+                ) {
+                    Text(
+                        text = "Phone",
+                        color = LightOrange,
+                        modifier = Modifier.weight(3f),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
+                    )
+                    Text(
+                        text = "Email",
+                        color = LightOrange,
+                        modifier = Modifier.weight(2f),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
                     )
                 }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(75.dp, 0.dp, 50.dp, 0.dp)
+                ) {
+                    ClickableText(
+                        text = AnnotatedString(people.phoneNumber),
+
+                        modifier = Modifier.weight(3f),
+                        style = TextStyle(color = if (isSystemInDarkTheme())
+                            Color.LightGray
+                        else
+                            Color.Black)
+                    ) {
+                        ContextCompat.startActivity(
+                            context,
+                            Intent(
+                                Intent.ACTION_DIAL,
+                                Uri.parse("tel:" + people.phoneNumber)),
+                            null
+                        )
+
+                    }
+                    ClickableText(
+                        text = AnnotatedString(people.email),
+
+                        modifier = Modifier.weight(3f),
+                        style = TextStyle(color = if (isSystemInDarkTheme())
+                            Color.LightGray
+                        else
+                            Color.Black)
+                    ) {
+                        ContextCompat.startActivity(
+                            context,
+                            Intent(
+                                Intent.ACTION_SENDTO,
+                                Uri.parse("mailto:" + people.email)
+                            ), null
+                        )
+
+                    }
+                }
+                Spacer(modifier = Modifier.size(20.dp))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(75.dp, 20.dp, 50.dp, 10.dp)
+                ) {
+                    Text(
+                        text = "Address",
+                        color = LightOrange,
+                        modifier = Modifier.weight(3f),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
+                    )
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(75.dp, 0.dp, 50.dp, 0.dp)
+                ) {
+                    Text(
+                        text = "${people.address}, ${people.state} ${people.country}",
+                        color = if (isSystemInDarkTheme()) Color.LightGray else Color.Black,
+                        modifier = Modifier.weight(3f)
+                    )
+                }
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = "Logo",
+                    modifier = Modifier
+                        .size(150.dp)
+                        .padding(75.dp, 0.dp, 0.dp, 0.dp)
+                )
             }
         }
-    )
+    }
 }
